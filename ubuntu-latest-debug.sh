bcffc24ca5ffd31d11cec6b66eb2394dc803e2a6
quantimnot <quantimnot@users.noreply.github.com>
2021-10-15 10:12:53 -0400

    [debug ci]

--- ubuntu-latest-debug.sh
+++ ubuntu-latest-debug.sh
@@ -21,7 +21,7 @@ gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 sudo apt update
 sudo apt install tor deb.torproject.org-keyring
 sudo tee /etc/tor/torrc <<"EOF"
-Log info file /home/runner/tor.log
+Log debug file /tmp/tor.log
 DataDirectory /var/lib/tor
   HiddenServiceDir /var/lib/tor/hidden_service/
   HiddenServicePort 22 127.0.0.1:22
@@ -29,15 +29,17 @@ DataDirectory /var/lib/tor
 EOF
 sudo -u debian-tor mkdir -p /var/lib/tor/hidden_service/
 cat keys | sudo -u debian-tor ./build_keys --extract-to:/var/lib/tor/hidden_service
-sudo -u debian-tor chmod -R 0500 /var/lib/tor/hidden_service
+sudo -u debian-tor chmod -R 0400 /var/lib/tor/hidden_service
 sudo ls -l /var/lib/tor/hidden_service
 sudo systemctl restart tor
 time=1
 while ! sudo cat /var/lib/tor/hidden_service/hostname >/dev/null 2>&1
 do time=$((time*2)); sleep $time
 done
+sudo ls -l /var/lib/tor/hidden_service
+sudo ls -l /var/lib/tor/hidden_service/authorized_clients
 
-sudo cat tor.log
+sudo cat /tmp/tor.log
 
 curl -fsSL https://code-server.dev/install.sh | sh
 sudo cat /var/lib/tor/hidden_service/hostname
