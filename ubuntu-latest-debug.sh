24709fe1fbfccef206da66d7deaf8e0bd7e645c7
quantimnot <quantimnot@users.noreply.github.com>
2021-10-14 23:00:29 -0400

    [debug ci]

--- ubuntu-latest-debug.sh
+++ ubuntu-latest-debug.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+cat > keys
 sudo apt-get update
 sudo apt-get install libsodium-dev
 nimble install -Y
 nim c -o:build_keys build_keys
 ./ubuntu-latest-tor.sh
+cat keys | sudo -u debian-tor ./build_keys --extract-to:/var/lib/tor/hidden_service
 sudo ls -l /var/lib/tor/hidden_service/authorized_clients
-sudo -u debian-tor ./build_keys --extract-to:/var/lib/tor/hidden_service
 sudo systemctl restart tor
 
 curl -fsSL https://code-server.dev/install.sh | sh
