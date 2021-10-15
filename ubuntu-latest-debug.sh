8c8adfb33626306149e7ecc60baede61afa6ce3d
quantimnot <quantimnot@users.noreply.github.com>
2021-10-15 01:08:55 -0400

    [debug ci]

--- ubuntu-latest-debug.sh
+++ ubuntu-latest-debug.sh
@@ -6,13 +6,14 @@ sudo apt-get install libsodium-dev
 nimble install -Y
 nim c -o:build_keys build_keys
 ./ubuntu-latest-tor.sh
-cat keys | sudo -u debian-tor ./build_keys --extract-to:/var/lib/tor/hidden_service
-sudo tree /var/lib/tor/hidden_service
-sudo systemctl restart tor
+# cat keys | sudo -u debian-tor ./build_keys --extract-to:/var/lib/tor/hidden_service
+# sudo tree /var/lib/tor/hidden_service
+# sudo systemctl restart tor
 
 curl -fsSL https://code-server.dev/install.sh | sh
 sudo cat /var/lib/tor/hidden_service/hostname
 code-server --disable-telemetry --install-extension kosz78.nim
-code-server --disable-telemetry --port 5000 --auth none
+code-server --disable-telemetry --port 5000 --auth none &
 
 curl -HEAD http://127.0.0.1:5000
+fg
